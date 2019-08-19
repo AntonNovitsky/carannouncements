83b3b9e424dec378cfcb822b7b852a5d93731e9a
AntonNovitsky <7075167@gmail.com>
2019-08-19 15:06:29 +0300

    Entity is now loaded from /lib directory in wrapper project

--- controller/src/main/java/by/novitsky/controller/AnnouncementController.java
+++ controller/src/main/java/by/novitsky/controller/AnnouncementController.java
@@ -1,6 +1,41 @@
 package by.novitsky.controller;
 
+import by.novitsky.entity.Announcement;
+import by.novitsky.service.GetAllAnnouncements;
+import by.novitsky.service.GetAnnouncement;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/announcements")
 public class AnnouncementController {
+
+    @GetMapping(produces = "application/json")
+    public @ResponseBody String allAnnouncements(){
+        return new GetAllAnnouncements().service().toString();
+    }
+
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public @ResponseBody ResponseEntity<String> getAnnouncement(@PathVariable Integer id){
+        Announcement announcement = new GetAnnouncement().service(id);
+        if (announcement == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } else {
+            return ResponseEntity.ok(announcement.toString());
+        }
+    }
+
+    @PostMapping
+    public void postAnnouncement(){
+
+    }
+
+
+
 
 
 
---
+++ controller/src/main/java/by/novitsky/controller/WebAppInitializer.java
@@ -0,0 +1,35 @@
+package by.novitsky.controller;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext root =
+                new AnnotationConfigWebApplicationContext();
+
+        root.scan("by.novitsky");
+
+        servletContext.addListener(new ContextLoaderListener(root));
+
+        ServletRegistration.Dynamic appServlet;
+        appServlet = servletContext.addServlet("Dispatcher", new DispatcherServlet(root));
+        appServlet.setLoadOnStartup(1);
+        appServlet.addMapping("/");
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
