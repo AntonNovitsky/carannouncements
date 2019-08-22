9787e04da0d7afe1aa8a185970a41a3b1384d9ec
AntonNovitsky <7075167@gmail.com>
2019-08-22 11:27:16 +0300

    Deleted files left from web application

--- src/main/java/by/novitsky/Main.java
+++
@@ -1,9 +0,0 @@
-package by.novitsky;
-
-import by.novitsky.cofiguration.ConfigurationManager;
-
-public class Main {
-  public static void main(String[] args) {
-    System.out.println(ConfigurationManager.getProperty("db.url"));
-  }
-}
